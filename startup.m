45904da3088aaed60ba2c006e66736e600b10da0
Byungjun Kim <56605022+pontstrasse12@users.noreply.github.com>
2023-01-09 14:16:18 -0800

    first commit

---
+++ startup.m
@@ -0,0 +1,5 @@
+addpath(genpath('/Users/byungjun/00-codes/23-cyclo_modul_OFDM/'))
+disp('cyclo_modul_OFDM is added to path')
+
+addpath(genpath('/Users/byungjun/00-codes/signalGenerator/'))
+disp('signalGenerator is added to path')
