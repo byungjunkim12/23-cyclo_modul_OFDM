aa2f97a50c38d2634d66bbabb37a54f1663333ef
pontstrasse12 <byungjun.kim12@gmail.com>
2023-01-26 14:39:18 -0800

    getMCS_HEMU

---
+++ python/utilities.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def getMCS_HEMU(inputJson):
+    nUser = len(inputJson['sourceArray'][0]['signalArray']['cfgHEMU']['User'])
+    MCS_list = np.zeros((nUser,), dtype=int)
+    for iUser in range(nUser):
+        MCS_list[iUser] = inputJson['sourceArray'][0]['signalArray']['cfgHEMU']['User'][iUser]['MCS']
+
+    return MCS_list
