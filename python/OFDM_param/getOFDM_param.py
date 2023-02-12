db9c7980fcce3f89d9564afefa8ac173287d26d3
pontstrasse12 <byungjun.kim12@gmail.com>
2023-02-11 16:07:59 -0800

    functionize OFDM_param

---
+++ python/OFDM_param/getOFDM_param.py
@@ -0,0 +1,46 @@
+import numpy as np
+from scipy import signal
+
+def getOFDM_param(inputIQ):
+    FFTsize = 4096
+    tauVec = np.array([64, 256, 333, 667, 1333])
+    freqBinList = [[51, 57], [13, 14, 15], [10, 12]]
+    symLenList = np.array([[80, 72], [320, 288, 272], [640, 548]], dtype=object)
+    inputLen = 8
+    
+    CAF_DC = getCAF_DC(inputIQ, tauVec, inputLen)
+    nSubC_Est = tauVec[np.argmax(CAF_DC)]
+    CAF = getCAF(inputIQ, FFTsize, nSubC_Est, inputLen)
+    symLenEst = symLenList[np.argmax(CAF_DC)][np.argmax(CAF[freqBinList[np.argmax(CAF_DC)]])]
+
+    return nSubC_Est, symLenEst
+
+def getCAF_DC(inputIQ, tauVec, CPLen):
+    indexMat = np.arange(1, CPLen + 1).reshape(-1, 1) + np.arange(0, len(inputIQ) - CPLen)
+    inputMat = inputIQ[indexMat]
+    CAF_DC = np.zeros(len(tauVec))
+
+    for tauIndex in range(len(tauVec)):
+        tau = tauVec[tauIndex]
+
+        if CPLen == 1:
+            inputCorr = inputMat[:-tau] * np.conj(inputMat[tau:])
+        else:
+            inputCorr = np.sum(inputMat[:, :-tau] * np.conj(inputMat[:, tau:]), axis=1)
+        CAF_DC[tauIndex] = np.mean(np.abs(inputCorr))
+    return CAF_DC
+
+def getCAF(inputIQ, FFTsize, tau, CPLen):
+    indexMat = np.arange(1, CPLen + 1).reshape(-1, 1) + np.arange(0, len(inputIQ) - CPLen)
+    inputMat = inputIQ[indexMat]
+
+    if CPLen == 1:
+        inputCorr = inputMat[:-tau] * np.conj(inputMat[tau:])
+    else:
+        inputCorr = np.sum(inputMat[:, :-tau] * np.conj(inputMat[:, tau:]), axis=0)
+
+    inputSpec = np.zeros((FFTsize, len(inputCorr) - FFTsize + 1), dtype=complex)
+    for i in range(inputSpec.shape[1]):
+        inputSpec[:, i] = np.fft.fft(inputCorr[i : i+FFTsize])
+    CAF = np.mean(np.abs(inputSpec), axis=1)
+    return CAF
--- python/utilities.py
+++ python/utilities.py
@@ -8,34 +8,3 @@ def getMCS_HEMU(inputJson):
 
     return MCS_list
 
-def getCAF_DC(inputIQ, tauVec, CPLen):
-    indexMat = np.arange(1, CPLen + 1).reshape(-1, 1) + np.arange(0, len(inputIQ) - CPLen)
-    inputMat = inputIQ[indexMat]
-    CAF_DC = np.zeros(len(tauVec))
-
-    for tauIndex in range(len(tauVec)):
-        tau = tauVec[tauIndex]
-
-        if CPLen == 1:
-            inputCorr = inputMat[:-tau] * np.conj(inputMat[tau:])
-        else:
-            inputCorr = np.sum(inputMat[:, :-tau] * np.conj(inputMat[:, tau:]), axis=1)
-        CAF_DC[tauIndex] = np.mean(np.abs(inputCorr))
-    return CAF_DC
-
-def getCAF(inputIQ, FFTsize, tau, CPLen):
-    indexMat = np.arange(1, CPLen + 1).reshape(-1, 1) + np.arange(0, len(inputIQ) - CPLen)
-    inputMat = inputIQ[indexMat]
-
-    if CPLen == 1:
-        inputCorr = inputMat[:-tau] * np.conj(inputMat[tau:])
-    else:
-        inputCorr = np.sum(inputMat[:, :-tau] * np.conj(inputMat[:, tau:]), axis=0)
-
-    inputSpec = np.zeros((FFTsize, len(inputCorr) - FFTsize + 1))
-    for i in range(inputSpec.shape[1]):
-        inputSpec[:, i] = np.fft.fft(inputCorr[i : i+FFTsize])
-    CAF = np.mean(np.abs(inputSpec), axis=1)
-    return CAF
-
-
