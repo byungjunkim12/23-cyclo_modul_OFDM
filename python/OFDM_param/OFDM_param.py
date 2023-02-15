b2806141132678616d7ee0d40c0289cce66388f4
pontstrasse12 <byungjun.kim12@gmail.com>
2023-02-15 13:56:27 -0800

    functionize OFDM_param

--- python/OFDM_param/OFDM_param.py
+++ python/OFDM_param/OFDM_param.py
@@ -2,12 +2,15 @@ import sys
 import numpy as np
 import json
 import argparse
+import random
+import time
+
+import os
 from glob import glob
 
-sys.path.append('../')
-
-from utilities import *
 from getOFDM_param import *
+sys.path.append('../')
+from utilities import *
 
 def main():
     parser = argparse.ArgumentParser()
@@ -16,9 +19,10 @@ def main():
     inputJsonFileName = args.inputJson
 
     inputJsonFile = open("../inputJson/" + inputJsonFileName + ".json")
-    data_path = json.load(inputJsonFile)["data_path"]
-    dirFilenameList = glob(data_path + '/*.32cf')
-    
+    dataPath = json.load(inputJsonFile)["data_path"]
+    inputJsonFile.close()
+    # dirFilenameList = glob(data_path + '/*.32cf')
+
     SNRVec = np.arange(0, 21, 2, dtype=int)
     # SNRVec = np.arange(100, 101, 2, dtype=int)
     tauVec = np.array([64, 256, 333, 667, 1333])
@@ -26,38 +30,81 @@ def main():
 
     CPLenList = np.array([np.array(['Extended', 'Normal']),\
         np.array(['Extended', 'Medium', 'Normal']),\
-        np.array(['Extended', 'Normal'])], dtype=object)
-    symLenList = np.array([[80, 72], [320, 288, 272], [640, 548]], dtype=object)
+        np.array(['Extended', 'Normal']),\
+        np.array(['Normal']), np.array(['Normal'])], dtype=object)
+    symLenList = np.array([[80, 72], [320, 288, 272], [640, 548], [1096], [2192]], dtype=object)
     
     preambleLen = 1200
+    maxInputLen = 20000
     
-    fileCount = np.zeros((len(SNRVec), len(protocolList)))
-    corrCount = np.zeros((len(SNRVec), len(protocolList)))
-    for dirFileName in dirFilenameList:
-        fileName = dirFileName.split("/")[-1][:-5]
-        fileNameSplit = fileName.split("_")
-        fileProtocol = fileNameSplit[0]
-        fileCPLen = fileNameSplit[1]
-        fileSNR = int(fileNameSplit[4])
+    fileCount = np.zeros((len(protocolList),), dtype=object)
+    corrNSubCCount = np.zeros((len(protocolList),), dtype=object)
+    corrSymLenCount = np.zeros((len(protocolList),), dtype=object)
+    prevSaveFileName = "../result/start.npy"
+    for i in range(len(protocolList)):
+        fileCount[i] = np.zeros((len(SNRVec), CPLenList[i].shape[0]))
+        corrNSubCCount[i] = np.zeros((len(SNRVec), ))
+        corrSymLenCount[i] = np.zeros((len(SNRVec), CPLenList[i].shape[0]))
 
-        fileProtocolIndex = np.argwhere(fileProtocol == protocolList)[0][0]
-        fileSymLen = symLenList[fileProtocolIndex]\
-            [np.argwhere(fileCPLen == CPLenList[fileProtocolIndex])[0][0]]
-        # print(fileProtocol, fileCPLen, fileSNR, fileSymLen)
+    countSave = np.concatenate((np.expand_dims(fileCount, axis=0),\
+            np.expand_dims(corrNSubCCount, axis=0),\
+            np.expand_dims(corrSymLenCount, axis=0)), axis=0)
+    with open(prevSaveFileName, 'wb') as saveFile:
+        np.save(saveFile, countSave)
 
-        load_out = np.fromfile(dirFileName, dtype=np.float32)
-        data = load_out[np.arange(0, load_out.shape[0], 2)] +\
-                            1j * load_out[np.arange(1, load_out.shape[0], 2)]
-        inputIQ = data[preambleLen:]
+    for subFolder in os.walk(dataPath):
+        startTime = time.time()
+        dirFilenameList = glob(subFolder[0] + '/*.32cf')
+        if len(dirFilenameList) == 0:
+            continue
 
-        nSubC_Est, symLenEst = getOFDM_param(inputIQ)
+        print(subFolder[0].split("/")[-2] + "_" + subFolder[0].split("/")[-1])
+
+        for dirFileName in dirFilenameList:
+            fileName = dirFileName.split("/")[-1][:-5]
+            fileNameSplit = fileName.split("_")
+            fileProtocol = fileNameSplit[0]
+            fileCPLen = fileNameSplit[1]
+            fileSNR = int(fileNameSplit[4])
+
+            fileProtocolIndex = np.argwhere(fileProtocol == protocolList)[0][0]
+            fileSymLen = symLenList[fileProtocolIndex]\
+                [np.argwhere(fileCPLen == CPLenList[fileProtocolIndex])[0][0]]
+            fileSymLenIndex = np.argwhere(fileCPLen == CPLenList[fileProtocolIndex])[0][0]
+            fileSNRIndex = np.argwhere(SNRVec == fileSNR)[0][0]
+            # print(fileProtocol, fileCPLen, fileSNR, fileSymLen)
+
+            load_out = np.fromfile(dirFileName, dtype=np.float32)
+            data = load_out[np.arange(0, load_out.shape[0], 2)] +\
+                                1j * load_out[np.arange(1, load_out.shape[0], 2)]
+            inputIQ = data[preambleLen:]
+            if inputIQ.shape[0] > maxInputLen:
+                randStartIndex = random.randint(0, inputIQ.shape[0] - maxInputLen)
+                inputIQ = inputIQ[randStartIndex : randStartIndex + maxInputLen]
+
+            nSubC_Est, symLenEst = getOFDM_param(inputIQ)
+            
+            fileCount[fileProtocolIndex][fileSNRIndex, fileSymLenIndex] += 1
+            if nSubC_Est == tauVec[fileProtocolIndex]:
+                corrNSubCCount[fileProtocolIndex][fileSNRIndex] += 1
+            if nSubC_Est == tauVec[fileProtocolIndex] and symLenEst == fileSymLen:
+                corrSymLenCount[fileProtocolIndex][fileSNRIndex, fileSymLenIndex] += 1
         
-        fileCount[np.argwhere(SNRVec == fileSNR)[0][0], fileProtocolIndex] += 1        
-        if nSubC_Est == tauVec[fileProtocolIndex] and symLenEst == fileSymLen:
-            corrCount[np.argwhere(SNRVec == fileSNR)[0][0], fileProtocolIndex] += 1
+        # print(fileCount)
+        # print(corrNSubCCount)
+        # print(corrSymLenCount)
+        print('running time: %s sec' %(time.time() - startTime))
 
-    print(fileCount)
-    print(corrCount)
+        countSave = np.concatenate((np.expand_dims(fileCount, axis=0),\
+            np.expand_dims(corrNSubCCount, axis=0),\
+            np.expand_dims(corrSymLenCount, axis=0)), axis=0)
+        print(countSave)
+        saveFileName = '../result/' + subFolder[0].split("/")[-2] + "_" +\
+            subFolder[0].split("/")[-1] + ".npy"
+        os.rename(prevSaveFileName, saveFileName)
+        with open(saveFileName, 'wb') as saveFile:
+            np.save(saveFile, countSave)
+        prevSaveFileName = saveFileName
 
 if __name__ == "__main__":
     main()
--- python/OFDM_param/getOFDM_param.py
+++ python/OFDM_param/getOFDM_param.py
@@ -4,8 +4,8 @@ from scipy import signal
 def getOFDM_param(inputIQ):
     FFTsize = 4096
     tauVec = np.array([64, 256, 333, 667, 1333])
-    freqBinList = [[51, 57], [13, 14, 15], [10, 12]]
-    symLenList = np.array([[80, 72], [320, 288, 272], [640, 548]], dtype=object)
+    freqBinList = [[51, 57], [13, 14, 15], [10, 12], [0], [0]]
+    symLenList = np.array([[80, 72], [320, 288, 272], [640, 548], [1096], [2192]], dtype=object)
     inputLen = 8
     
     CAF_DC = getCAF_DC(inputIQ, tauVec, inputLen)
@@ -22,25 +22,28 @@ def getCAF_DC(inputIQ, tauVec, CPLen):
 
     for tauIndex in range(len(tauVec)):
         tau = tauVec[tauIndex]
-
         if CPLen == 1:
             inputCorr = inputMat[:-tau] * np.conj(inputMat[tau:])
         else:
             inputCorr = np.sum(inputMat[:, :-tau] * np.conj(inputMat[:, tau:]), axis=1)
         CAF_DC[tauIndex] = np.mean(np.abs(inputCorr))
+
     return CAF_DC
 
 def getCAF(inputIQ, FFTsize, tau, CPLen):
     indexMat = np.arange(1, CPLen + 1).reshape(-1, 1) + np.arange(0, len(inputIQ) - CPLen)
     inputMat = inputIQ[indexMat]
+    SpecStep = 7
 
     if CPLen == 1:
         inputCorr = inputMat[:-tau] * np.conj(inputMat[tau:])
     else:
         inputCorr = np.sum(inputMat[:, :-tau] * np.conj(inputMat[:, tau:]), axis=0)
 
-    inputSpec = np.zeros((FFTsize, len(inputCorr) - FFTsize + 1), dtype=complex)
-    for i in range(inputSpec.shape[1]):
-        inputSpec[:, i] = np.fft.fft(inputCorr[i : i+FFTsize])
+    inputSpecRange = np.arange(0, len(inputCorr) - FFTsize, SpecStep)
+    inputSpec = np.zeros((FFTsize, inputSpecRange.shape[0]), dtype=complex)
+    for i in range(inputSpecRange.shape[0]):
+        corrIndex = inputSpecRange[i]
+        inputSpec[:, i] = np.fft.fft(inputCorr[corrIndex : corrIndex+FFTsize])
     CAF = np.mean(np.abs(inputSpec), axis=1)
     return CAF
