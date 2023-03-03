3864080632a1ec10a8886aaf67ffd81c3f198eef
pontstrasse12 <byungjun.kim12@gmail.com>
2023-03-02 21:58:08 -0800

    change saving filename format

--- python/CFOcorr/util_CFOcorr.py
+++ python/CFOcorr/util_CFOcorr.py
@@ -1,17 +1,5 @@
 import math
 import numpy as np
-from scipy.signal import find_peaks
-
-def findFirstIndex(inputIQ, nSubC, CPLen):
-    indexMat = np.arange(CPLen)[:, np.newaxis] + np.arange(inputIQ.shape[0]-CPLen+1)
-    inputMat = inputIQ[indexMat]
-    
-    inputCorr = np.sum(inputMat[:, :-nSubC] * np.conj(inputMat[:, nSubC:]), axis=0)
-    peaks, pklocs = find_peaks(np.abs(inputCorr), distance=np.floor(0.9*(nSubC+CPLen)))
-    print(np.mod(np.bincount(np.mod(peaks, nSubC+CPLen)).argmax(), nSubC+CPLen))
-    firstIndex = np.mod(np.bincount(np.mod(peaks, nSubC+CPLen)).argmax(), nSubC+CPLen)
-
-    return firstIndex
 
 def estCFO(inputIQ, nSubC, lenCP, firstIndexSym, samplingRate):
     lenOFDMSym = nSubC + lenCP
